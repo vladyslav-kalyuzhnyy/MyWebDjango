1e1c23831718744cf40729f861f6427cc472f076
Vladyslav Kalyuzhnyy <vladyslav.kalyuzhnyy@gmail.com>
2020-02-09 16:46:21 +0200

    Model List/Detail Views

--- filmcatalog/views.py
+++ filmcatalog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Movies List"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    context_object_name = "movie_list"
+    queryset = Movie.objects.filter(draft=False)
+    template_name = "movies/movie_list.html"
 
 
-
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = "url"
+    template_name = "movies/movie_detail.html"
