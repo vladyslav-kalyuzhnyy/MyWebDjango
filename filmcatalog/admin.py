a616fb822033770062c67aee3a4de5b2577259b0
Vladyslav Kalyuzhnyy <vladyslav.kalyuzhnyy@gmail.com>
2020-02-14 01:30:29 +0200

    AdminPanel updates

--- filmcatalog/admin.py
+++ filmcatalog/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "url"]
+    list_display_links = ("name",)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "url", "draft"]
+    list_filter = ("category", "year")
+    list_display_links = ("title", "url")
+    search_fields = ("title", "category__name")
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+
+
 admin.site.register(Genre)
-admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
