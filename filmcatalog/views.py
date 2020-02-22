2070c4cbad0c63f8c56b607301afea110d63d742
Vladyslav Kalyuzhnyy <vladyslav.kalyuzhnyy@gmail.com>
2020-02-22 17:58:07 +0200

    movies filter sidebar

--- filmcatalog/views.py
+++ filmcatalog/views.py
@@ -1,19 +1,28 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie, Actor
+from .models import Movie, Actor, Genre
 from .forms import ReviewForm
 
 
-class MoviesView(ListView):
+class GenreYear:
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
     """Movies List"""
     context_object_name = "movie_list"
     queryset = Movie.objects.filter(draft=False)
     template_name = "movies/movie_list.html"
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Movie Details"""
     model = Movie
     slug_field = "url"
@@ -33,7 +42,18 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+    template_name = 'movies/movie_list.html'
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
