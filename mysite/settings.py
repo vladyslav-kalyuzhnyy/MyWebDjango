31032101e9e6b1636dcdbad9c091581b6b0f82b6
Vladyslav Kalyuzhnyy <vladyslav.kalyuzhnyy@gmail.com>
2020-03-23 17:01:34 +0200

    Simple SignIn and SignUp with AllAuth.

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'filmcatalog',
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -87,6 +89,12 @@ DATABASES = {
 }
 
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -119,6 +127,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('filmcatalog.urls')),
 ]
 
